77bb6c848f8077a76a21f5665c03db81572a2faa
hintonbradley <hintonbradley@gmail.com>
2019-11-17 14:51:47 -0800

    adding api

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
+import keys from '../unsplash';
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log('from App component: ', term);
+    state = {
+        images: []
+    }
+
+    // Using async and await instead of .then to hold http response. Using a special arrow function with async in order to keep this context.
+    onSearchSubmit = async (term) => {
+        const resp = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID ' + keys.accessKey
+            }
+        })
+        
+        this.setState({ images: resp.data.results})
+
     }
 
     render () {
@@ -12,6 +29,7 @@ class App extends React.Component {
             <div>
                 {/* Passing method as props that will be used as a callback in SearchBar comoopnent */}
                 < SearchBar onSubmit={this.onSearchSubmit} />
+                <p>There are {this.state.images.length} images.</p>
             </div>
         )
     }
